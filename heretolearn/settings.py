e6cc9ec7bf8db91a08f742ff3890f9133493e5fd
Blake <blake.enyart@gmail.com>
2019-05-23 21:38:41 -0600

    Modifications for pushing to heroku

--- heretolearn/settings.py
+++ heretolearn/settings.py
@@ -24,7 +24,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 )
 
 # Quick-start development settings - unsuitable for production
