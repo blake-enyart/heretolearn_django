d05dad1acc6b8fb8e1060b70b5d4878b4f729d29
Blake <blake.enyart@gmail.com>
2019-05-23 20:30:24 -0600

    drop cruft of old tutorial app and added machinelearning app

--- heretolearn/settings.py
+++ heretolearn/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
-    'heretolearnapp',
+    # 'heretolearnapp.apps.HeretolearnappConfig',
+    'machinelearning.apps.MachinelearningConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- heretolearn/urls.py
+++ heretolearn/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('polls/', include('heretolearnapp.urls')),
+    # path('heretolearnapp', include('heretolearnapp.urls')),
+    path('', include('machinelearning.urls')),
     path('admin/', admin.site.urls),
 ]
